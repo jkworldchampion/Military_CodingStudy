15a73b4b2eaec921ed8b2bb31a95b3ff8130bd8a
JuHwan_Park <jkworldchampion@ajou.ac.kr>
2022-07-24 20:44:17 +0900

    *로 x자 만들기

---
+++ C_study/pr06_2.c
@@ -0,0 +1,18 @@
+    #include <stdio.h>
+
+int main(void){
+	for (int i=0; i<5; i++){
+		for (int j=0; j<5; j++){
+			if (i == j){
+				printf("*");
+			} else if (i+j == 4){
+				printf("*");
+			} else{
+				printf(" ");
+			}
+			
+		}
+		printf("\n");
+	}
+	return 0;
+}
