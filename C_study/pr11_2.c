564792cd30b70f53d4db42a889f199916ec1f507
JuHwan_Park <jkworldchampion@ajou.ac.kr>
2022-07-29 23:51:10 +0900

    getchar()이용 소문자 개수세기

---
+++ C_study/pr11_2.c
@@ -0,0 +1,18 @@
+# include <stdio.h>
+
+int main(void){
+	int ch;
+	int cnt = 0;
+	ch = getchar();
+	
+	while (ch != '\n'){
+		if (('a'<=ch) &&(ch <= 'z')){
+			cnt++;
+		} 
+		ch = getchar();
+	}
+	
+	printf("소문자의 개수 : %d\n", cnt);
+	
+	return 0;
+}
