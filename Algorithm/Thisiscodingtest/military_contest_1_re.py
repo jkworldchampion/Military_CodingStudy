e828d9e4e2f7f54fa0882b6c57e4a4b45058d3e0
JuHwan_Park <jkworldchampion@ajou.ac.kr>
2022-09-06 20:56:39 +0900

    Update military_contest_1_re.py

--- Algorithm/Thisiscodingtest/military_contest_1_re.py
+++ Algorithm/Thisiscodingtest/military_contest_1_re.py
@@ -2,15 +2,24 @@ def solution(h, k, boxes): # h 높이, k 넘어갈 수 있는 높이, boxes 박�
     boxes.sort()  # 박스 정렬
     now_height = 0 # 현재의 높이
     count = 0
-    # 바로 끝나는 위치인가 확인
-    
-
+    while True:
+        # 바로 끝나는 위치인가 확인
+        if h - now_height <= k:
+            return count
+        else:
+            if now_height == boxes[where_can_go(k, now_height, boxes)]:
+                return -1
+            else :
+                now_height = boxes[where_can_go(k, now_height, boxes)]
+                count += 1
 
 # 갈 수 있는 가장 높은 박스의 index를 반환
 def where_can_go(k, now, boxes):
     box = 0
     can = 0
     while(True):
+        if box >= len(boxes):
+            break
         if boxes[box] - now <= k:
             can = box
             box += 1
@@ -19,7 +28,7 @@ def where_can_go(k, now, boxes):
     return can
 
 
-# print(solution(20, 18, [1]))
-# print()
-# print(solution(12,3,[2,3,6,7,8,10,11]))
-# print(solution(10,1,[9,8,7,6,5,4,3,2,1]))  #여기서 잘못됨
+print(solution(20, 18, [1]))
+print()
+print(solution(12,3,[2,3,6,7,8,10,11]))
+print(solution(10,1,[9,8,7,6,5,4,3,2,1]))  #여기서 잘못됨
