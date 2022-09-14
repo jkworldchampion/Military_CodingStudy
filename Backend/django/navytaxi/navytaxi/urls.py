8af98131fe0896e9030fd23f66914d0f24507bea
JuHwan_Park <jkworldchampion@ajou.ac.kr>
2022-09-14 16:57:33 +0900

    Create urls.py

---
+++ Backend/django/navytaxi/navytaxi/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path, include
+from navytaxi import views
+from .views import helloAPI
+
+urlpatterns = [
+    path('', views.index),
+    path('hello/', helloAPI)
+]
